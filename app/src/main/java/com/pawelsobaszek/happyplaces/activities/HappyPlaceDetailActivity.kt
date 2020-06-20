0bf1b9457e19d637ee2c586d77a26d27848639e3
Paweł Sobaszek <szczowio@gmail.com>
2020-06-20 08:40:47 +0200

    second commit for PC

---
+++ app/src/main/java/com/pawelsobaszek/happyplaces/activities/HappyPlaceDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.pawelsobaszek.happyplaces.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pawelsobaszek.happyplaces.R
+
+class HappyPlaceDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_happy_place_detail)
+    }
+}
--- app/src/main/java/com/pawelsobaszek/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/pawelsobaszek/happyplaces/activities/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.pawelsobaszek.happyplaces.activities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pawelsobaszek.happyplaces.R
+import com.pawelsobaszek.happyplaces.adapters.HappyPlacesAdapter
 import com.pawelsobaszek.happyplaces.database.DatabaseHandler
 import com.pawelsobaszek.happyplaces.models.HappyPlaceModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,10 +21,19 @@ class MainActivity : AppCompatActivity() {
 
         fabAddHappyPlace.setOnClickListener {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
-            startActivity(intent)
+            startActivityForResult(intent, ADD_PLACE_ACTIVITY_REQUEST_CODE)
         }
 
         getHappyPlacesListFromLocalDB()
+    }
+
+    private fun setupHappyPlacesRecyclerView(happyPlaceList: ArrayList<HappyPlaceModel>) {
+        rv_happy_places_list.layoutManager = LinearLayoutManager(this)
+        rv_happy_places_list.setHasFixedSize(true)
+
+        val placesAdapter = HappyPlacesAdapter(this, happyPlaceList)
+        rv_happy_places_list.adapter = placesAdapter
+
     }
 
     private fun getHappyPlacesListFromLocalDB() {
@@ -28,10 +41,27 @@ class MainActivity : AppCompatActivity() {
         val getHappyPlaceList : ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
 
         if (getHappyPlaceList.size > 0) {
-            for (i in getHappyPlaceList) {
-                Log.e("Title", i.title)
-                Log.e("Description", i.description)
+            rv_happy_places_list.visibility = View.VISIBLE
+            tv_no_records_available.visibility = View.GONE
+            setupHappyPlacesRecyclerView(getHappyPlaceList)
+        } else {
+            rv_happy_places_list.visibility = View.GONE
+            tv_no_records_available.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ADD_PLACE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                getHappyPlacesListFromLocalDB()
+            } else {
+                Log.e("Activity", "Cancelled or Back pressed")
             }
         }
+    }
+
+    companion object {
+        var ADD_PLACE_ACTIVITY_REQUEST_CODE = 1
     }
 }
